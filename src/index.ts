fc721278753d0e101d4aa4eee17b7b9cd996fd81
vitorbisterso <vitorbisterso@gmail.com>
2022-11-01 10:59:25 -0300

    Add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-    res.json({
-        message: 'Welcome to the api'
-    });
+  res.json({
+    message: 'Welcome to the api',
+  });
 });
 
 app.listen(5000, () => console.log('Server started on port 5000'));
