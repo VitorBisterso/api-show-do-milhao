f2d46ec3057927e5f63411c9247979a9754314ba
totallynotfe <felipe_ramalheira@outlook.com>
2022-11-09 11:25:02 -0300

    feat: added category get route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 import UsersRoutes from '@/Features/Users';
+import CategoryRoutes from '@/Features/Category';
 import QuestionsRoutes from '@/Features/Questions';
 import { API_BASEPATH } from '@/Const';
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(`${API_BASEPATH}/users`, UsersRoutes);
+app.use(`${API_BASEPATH}/categories`, CategoryRoutes);
 app.use(`${API_BASEPATH}/questions`, QuestionsRoutes);
 
 app.listen(5000, () => console.log('Server started on port 5000'));
